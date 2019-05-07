f70511b970cef6951304d66ee71a1689083e0b09
Mitja Öhm <MitjaOehm@gmx.net>
2019-05-07 19:20:13 +0200

    Added Player with Jump and Walk

---
+++ FizbinTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	Animator animator;
+	Rigidbody2D rigbod2d;
+	SpriteRenderer spriteRenderer;
+	[SerializeField] Transform groundChecker;
+	bool isGrounded;
+
+	[SerializeField] float speedModifier;
+	[SerializeField] float jumpForce;
+	Vector2 jumpVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigbod2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        jumpVector = new Vector2 (0, jumpForce);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigbod2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speedModifier, rigbod2d.velocity.y);
+        if(Input.GetButton("Jump")) {
+        	rigbod2d.AddForce(jumpVector, ForceMode2D.Force);
+        }
+        if(Input.GetKey("space")) {
+        	Debug.Log("Jump around");
+        	rigbod2d.AddForce(jumpVector, ForceMode2D.Force);
+        }
+    }
+
+    private void GroundCheck () {
+    	if(Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Ground")))
+    		isGrounded = true;
+    	else 
+    		isGrounded = false;
+    }
+}
