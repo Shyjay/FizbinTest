ef515c0bb0111a6eb8bcca2e9a4922a428ad596e
Mitja Öhm <MitjaOehm@gmx.net>
2019-05-07 20:15:09 +0200

    Edited Platform Prefab for precision of collider

--- FizbinTest/Assets/Scripts/DeathCondition.cs
+++ FizbinTest/Assets/Scripts/DeathCondition.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathCondition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+   void OnCollisionEnter (Collision col)
     {
-        
+        if(col.gameObject.tag == "Player")
+        {
+        	Debug.Log("Death: reload Scene");
+            SceneManager.LoadScene(0);
+        }
     }
 }
